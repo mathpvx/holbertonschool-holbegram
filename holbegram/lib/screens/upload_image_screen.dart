c88067375aff74c2f49797032def41c3a642626a
mathpvx <7547@holbertonstudents.com>
2026-01-14 22:01:11 +0100

    basic stateless classes for login signup and upload image screens

---
+++ holbegram/lib/screens/upload_image_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddPicture extends StatelessWidget {
+  const AddPicture({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Upload Image Screen')),
+    );
+  }
+}
